484bf51dcb623bb758802e78bd1d07c57f0de12f
mariemk <101462688+MariemKhlif@users.noreply.github.com>
2023-10-27 15:50:30 +0200

    Add files via upload

---
+++ Tp3/src/transformation/BanqueService.java
@@ -0,0 +1,24 @@
+package transformation;
+
+import java.util.List;
+
+
+
+public class BanqueService {
+
+	
+	
+	public double conversion(double montant)
+	{
+		return montant*10.6;
+	}
+	
+	
+
+  public List <Compte>getComptes(List<Compte>comptes)
+	{
+	  return comptes;
+		
+	}
+	
+}
